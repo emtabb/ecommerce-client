b9cef68c0f0c44a70e36b43b63b2ad6f2618aa03
devnguyenduc <43005916+devnguyenduc@users.noreply.github.com>
2021-12-15 23:14:52 +0700

    Create networkSettings.sh

---
+++ networkSettings.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# insert/update hosts entry
+ip_address="35.206.235.151"
+host_name="espaceizanami-prod-db.asia-east1-b.c.emtabb"
+# find existing instances in the host file and save the line numbers
+matches_in_hosts="$(grep -n $host_name /etc/hosts | cut -f1 -d:)"
+host_entry="${ip_address} ${host_name}"
+
+echo "Please enter your password if requested."
+
+if [ ! -z "$matches_in_hosts" ]
+then
+    echo "Updating existing network."
+    # iterate over the line numbers on which matches were found
+    while read -r line_number; do
+        # replace the text of each line with the desired host entry
+        sudo sed -i '' "${line_number}s/.*/vpn /" /etc/hosts
+    done <<< "done"
+else
+    echo "Adding new vpn settings."
+fi
