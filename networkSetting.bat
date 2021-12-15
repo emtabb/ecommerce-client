caa2494dc26a94f1f6fa562cc02d1de1474b7c9a
devnguyenduc <43005916+devnguyenduc@users.noreply.github.com>
2021-12-15 23:05:43 +0700

    Create networkSetting.bat

---
+++ networkSetting.bat
@@ -0,0 +1,33 @@
+@echo off
+TITLE Modifying your HOSTS file
+COLOR F0
+ECHO.
+
+:LOOP
+SET Choice=
+SET /P Choice="Do you want to modify VPN NETWORK SETTINGS ? (Y/N)"
+
+IF NOT '%Choice%'=='' SET Choice=%Choice:~0,1%
+
+ECHO.
+IF /I '%Choice%'=='Y' GOTO ACCEPTED
+IF /I '%Choice%'=='N' GOTO REJECTED
+ECHO Please type Y (for Yes) or N (for No) to proceed!
+ECHO.
+GOTO Loop
+
+
+:REJECTED
+ECHO Your HOSTS file was left unchanged>>%systemroot%\Temp\hostFileUpdate.log
+ECHO Finished.
+GOTO END
+
+
+:ACCEPTED
+SET NEWLINE=^& echo.
+ECHO Carrying out requested modifications to your HOSTS file
+FIND /C /I "mydomain.com" %WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO %NEWLINE%>>%WINDIR%\system32\drivers\etc\hosts
+IF %ERRORLEVEL% NEQ 0 ECHO 35.206.235.151 espaceizanami-prod-db.asia-east1-b.c.emtabb>>%WINDIR%\system32\drivers\etc\hosts
+ECHO Finished
+GOTO END
