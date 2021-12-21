5a8a6efedf54a61271b5aceb1c485ae3f5709a3d
doantrung1197 <doantrung1197@gmail.com>
2021-12-21 23:12:12 +0700

    Change backgroud color footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PopulateBackgroundColor from "../facade/populate/PopulateBackgroundColor";
+import { Container, Row, Col } from "react-bootstrap";
 
 export default function Footer ({DEFAULT_COLOR, FOOTER_CONTACT, FOOTER_ADDRESS}) {
 
     return (
-        <footer className={"footer ".concat(PopulateBackgroundColor.populateFooter(DEFAULT_COLOR))} style={{height: "10rem"}}>
-            <div className="container">
+        <footer className={"footer ".concat(PopulateBackgroundColor.populateFooter(DEFAULT_COLOR))} style={{height: "10rem", backgroundColor: "#303030"}}>
+            {/* <div className="container">
                 <div className="row align-items-center justify-content-lg-between">
                     <div className="mb-lg-0 mb-4">
 
@@ -25,7 +26,14 @@ export default function Footer ({DEFAULT_COLOR, FOOTER_CONTACT, FOOTER_ADDRESS})
                         </ul>
                     </div>
                 </div>
-            </div>
+            </div> */}
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col>2 of 3</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
         </footer>
     );
 }
