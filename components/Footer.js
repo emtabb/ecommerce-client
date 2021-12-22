fbef1e82b75b4b92fbbaacd17384dd08e93c374b
doantrung1197 <doantrung1197@gmail.com>
2021-12-22 22:45:47 +0700

    [Update] Information in Footer page

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import PopulateBackgroundColor from "../facade/populate/PopulateBackgroundColor";
 import { Container, Row, Col } from "react-bootstrap";
+import { MdPlace } from "react-icons/md";
+import { GoMail } from "react-icons/go";
+import { AiFillPhone } from "react-icons/ai";
 
 export default function Footer ({DEFAULT_COLOR, FOOTER_CONTACT, FOOTER_ADDRESS}) {
 
@@ -27,11 +30,50 @@ export default function Footer ({DEFAULT_COLOR, FOOTER_CONTACT, FOOTER_ADDRESS})
                     </div>
                 </div>
             </div> */}
-            <Container>
+            <Container style={{color: "wheat"}}>
                 <Row>
-                    <Col>1 of 3</Col>
-                    <Col>2 of 3</Col>
-                    <Col>3 of 3</Col>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col>1 of 3</Col>
+                            </Row>
+                            <Row>
+                                <Col>2 of 3</Col>
+                            </Row>
+                            <Row>
+                                <Col>3 of 3</Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col>1 of 3</Col>
+                            </Row>
+                            <Row>
+                                <Col>2 of 3</Col>
+                            </Row>
+                            <Row>
+                                <Col>3 of 3</Col>
+                            </Row>
+                        </Container>
+                    </Col>
+                    <Col>
+                        <Container>
+                            <Row>
+                                <Col><h5>THÔNG TIN LIÊN HỆ</h5></Col>
+                            </Row>
+                            <Row>
+                                <Col><MdPlace></MdPlace> Văn Phòng 02 Tầng 8, Tòa Nhà Pearl Plaza, Số 561A Điện Biên Phủ, Phuờng 25, Quận Bình Thạnh, TP. Hồ Chí Minh</Col>
+                            </Row>
+                            <Row>
+                                <Col><GoMail></GoMail> thuykhieu@hqcons.vn</Col>
+                            </Row>
+                            <Row>
+                                <Col><AiFillPhone></AiFillPhone> 0973466970</Col>
+                            </Row>
+                        </Container>
+                    </Col>
                 </Row>
             </Container>
         </footer>
